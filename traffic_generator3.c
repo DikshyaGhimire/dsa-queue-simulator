eebef6170c957bd591f5843971e0ec0ce759d5dc
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-22 13:06:07 +0545

    traffic_generator3.c
    Add a traffic generator that logs vehicle IDs to a file.

--- traffic_generator3.c
+++ traffic_generator3.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+  int id = 2001;
+while(1){
+FILE *f = fopen("vehicles.data", "a");
+if(IF){
+printf("Error opening file\n");
+return 1;
+}
+fprintf(f, "D %d\n", id++);
+fclose(f);
+sleep(4);
+}
+return 0;
+}
