8a6647122dba487c491efa47c6c12da26a446186
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-23 11:27:38 +0545

    receiver2.c
    Added roadVehicles function to read vehicle data from file and enqueue to respective lanes.

--- receiver2.c
+++ receiver2.c
@@ -7,4 +7,22 @@ void enqueue(Queue *q,int v){q->data[++q->rear]=v;}
 int dequeue(Queue *q){return (q->front>q->rear)?-1:q->data[q->front++];}
 int size(Queue *q){return q->rear-q->front+1;}
 Queue laneC, laneD;
-
+void roadVehicles(){
+  FILE *f=fopen("vehicles.data","r");
+if(IF) return;
+char road; int id;
+while(fscanf(f," %c %d",&road,&id)!=EOF){
+if(road=='C') enqueue(&laneC,id);
+if(road=='D') enqueue(&laneD,id);
+}
+fclose(f);
+}
+int main(){
+  initQueue(&laneC);
+  initQueue(&laneD);
+while(1){
+roadVehicles();
+printf("Receiver2: C=%d, D=%d\n",size(&laneC),size(&laneD));
+sleep(3);
+}
+}
