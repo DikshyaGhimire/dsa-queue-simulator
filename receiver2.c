ddf9081f5f9af9f76a515e64571825cdc3678887
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-23 11:13:01 +0545

    receiver2.c

--- receiver2.c
+++ receiver2.c
@@ -1,1 +1,10 @@
+typedef struct {
+int data[MAX];
+int front, rear;
+} Queue;
+void initQueue(Queue *q){ q->front = 0; q->rear = -1;}
+void enqueue(Queue *q,int v){q->data[++q->rear]=v;}
+int dequeue(Queue *q){return (q->front>q->rear)?-1:q->data[q->front++];}
+int size(Queue *q){return q->rear-q->front+1;}
+Queue laneC, laneD;
 
