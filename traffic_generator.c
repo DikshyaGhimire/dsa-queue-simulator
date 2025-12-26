32cea3f0a03def124b20bc5c39959a5f4b20e088
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-26 21:10:54 +0545

    traffic_generator.c

--- traffic_generator.c
+++ traffic_generator.c
@@ -1,17 +1,1 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(){
-  int id = 1;
-while(1) {
-FILE *f = fopen("vehicles.data", "a");
-if(IF){
-printf("Error opening file\n");
-return 1;
-}
-fprintf(f, "A %d\n", id++);
-frprintf(f, "B %d\n", id++);
-fclose(f);
-sleep(2);
-}
-return 0;
-}
+
