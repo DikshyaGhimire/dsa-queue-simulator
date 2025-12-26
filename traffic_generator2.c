28a4200569eb9d7664f3c272753cb3cda9e456c9
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-26 21:18:07 +0545

    traffic_generator2.cpp

--- traffic_generator2.c
+++ traffic_generator2.c
@@ -1,16 +1,1 @@
-#include<stdio.h>
-#include<unistd.h>
-int main(){
-  int id = 1001; //start ID different from first generator
-while(1){
-FILE *f = fopen("Vehicles.data", "a");
-if(IF){
-printf("Error opening file\n");
-return 1;
-}
-fprintf(f, "C %d\n", id++);
-fclose(f);
-sleep(3); //slower generator
-}
-return 0;
-}
+
