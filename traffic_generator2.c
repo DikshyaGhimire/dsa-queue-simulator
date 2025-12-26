6d5f3bc34c21f2e5fcda79141b94ae64e49aabc3
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-26 21:22:24 +0545

    traffic_generator2.cpp

--- traffic_generator2.c
+++ traffic_generator2.c
@@ -1,1 +1,10 @@
-
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main(){
+  ofstream file("vehicles.data");
+if(!file) return 1;
+file << "A 12" << endl;
+file.close();
+return 0;
+}
