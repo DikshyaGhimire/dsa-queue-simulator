3ab400ae66588c7309508d0f978e795ffdfd37a0
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-26 21:17:17 +0545

    traffic_generator.c

--- traffic_generator.c
+++ traffic_generator.c
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main(){
+  ofstream file("Vehicles.data");
+if(!file) return 1;
+srand(time(0));
+file << "A" << rand() % 15 << endl;
+file << "B" << rand() % 10 << endl;
+file << "C" << rand() % 10 << endl;
+file << "D" << rand() % 10 << endl;
+file.close();
+return 0;
+}
