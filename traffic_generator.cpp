dd2c3ce072129829cbf6fe5e9ca2ae94a7047285
DikshyaGhimire <dikshyag24@gmail.com>
2025-12-27 12:37:15 +0545

    traffic_generator.cpp
    Refactored traffic generation to write to separate lane files.

--- traffic_generator.cpp
+++ traffic_generator.cpp
@@ -1,16 +1,24 @@
-#include<iostream>
 #include<fstream>
 #include<cstdlib>
 #include<ctime>
-using namespace std;
 int main(){
-  ofstream file("Vehicles.data");
-if(!file) return 1;
-srand(time(0));
-file << "A" << rand() % 15 << endl;
-file << "B" << rand() % 10 << endl;
-file << "C" << rand() % 10 << endl;
-file << "D" << rand() % 10 << endl;
-file.close();
+  std::srand(time(0));
+
+std::ofstream A("laneA.txt");
+std::ofstream B("laneB.txt");
+std::ofstream C("laneC.txt");
+std::ofstream D("laneD.txt");
+
+for(int i = 1; i<=50; i++){
+int r = rand() % 4;
+if(r == 0) A << i << "\n";
+if(r == 1) B << i << "\n";
+if(r == 2) C << i << "\n";
+if(r == 3) D << i << "\n";
+}
+A.close();
+B.close();
+C.close();
+D.close();
 return 0;
 }
